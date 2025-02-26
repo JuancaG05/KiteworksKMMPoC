0bc99066acbb8f464792adf6eb2420e2eabbd976
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-02-26 12:55:51 +0100

    refactor: changed dependency to interface instead of class in repository

--- shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/repository/FolderRepository.kt
+++ shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/repository/FolderRepository.kt
@@ -1,11 +1,11 @@
 package com.kiteworks.kiteworkskmmpoc.data.folder.repository
 
-import com.kiteworks.kiteworkskmmpoc.data.folder.datasources.FolderLocalDataSource
+import com.kiteworks.kiteworkskmmpoc.data.folder.datasources.IFolderLocalDataSource
 import com.kiteworks.kiteworkskmmpoc.domain.folder.Folder
 import com.kiteworks.kiteworkskmmpoc.domain.folder.repository.IFolderRepository
 
 class FolderRepository(
-    private val folderLocalDataSource: FolderLocalDataSource
+    private val folderLocalDataSource: IFolderLocalDataSource
 ): IFolderRepository {
 
     override suspend fun getAllFolders(): List<Folder> =

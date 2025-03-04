caa497c4948e4c361cd8eaa9882372dc15b96064
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-03-04 16:40:22 +0100

    refactor: remove unnecessary suspend

--- shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/domain/folder/usecases/GetAllFoldersUseCase.kt
+++ shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/domain/folder/usecases/GetAllFoldersUseCase.kt
@@ -2,12 +2,13 @@ package com.kiteworks.kiteworkskmmpoc.domain.folder.usecases
 
 import com.kiteworks.kiteworkskmmpoc.domain.folder.Folder
 import com.kiteworks.kiteworkskmmpoc.domain.folder.repository.IFolderRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetAllFoldersUseCase(
     private val folderRepository: IFolderRepository
 ) {
 
-    suspend fun execute(): List<Folder> =
+    fun execute(): Flow<List<Folder>> =
         folderRepository.getAllFolders()
 
 }

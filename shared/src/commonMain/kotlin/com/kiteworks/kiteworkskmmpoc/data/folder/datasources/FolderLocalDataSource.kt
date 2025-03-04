4805b092f904f7d2593d7844b4f534e2c6cf07b1
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-03-04 16:38:17 +0100

    feat: added operation to insert folder in local data source

--- shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/datasources/FolderLocalDataSource.kt
+++ shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/datasources/FolderLocalDataSource.kt
@@ -1,15 +1,33 @@
 package com.kiteworks.kiteworkskmmpoc.data.folder.datasources
 
+import app.cash.sqldelight.coroutines.asFlow
 import com.kiteworks.kiteworkskmmpoc.db.FolderLocalEntity
 import com.kiteworks.kiteworkskmmpoc.db.KiteworksKMMPoCDB
 import com.kiteworks.kiteworkskmmpoc.domain.folder.Folder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class FolderLocalDataSource(database: KiteworksKMMPoCDB): IFolderLocalDataSource {
 
     private val queries = database.folderQueries
 
-    override suspend fun getAllFolders(): List<Folder> =
-        queries.getAllFolders().executeAsList().map { it.toModel() }
+    override fun getAllFolders(): Flow<List<Folder>> =
+        queries.getAllFolders().asFlow().map { it.executeAsList().map { a -> a.toModel() }}
+
+    override fun insertFolder(folder: Folder) {
+        queries.insertFolder(
+            id = folder.id.toLong(),
+            name = folder.name,
+            userId = folder.userId.toLong(),
+            isShared = if (folder.isShared) 1L else 0L,
+            path = folder.path,
+            parentId = folder.parentId.toLong(),
+            type = folder.type,
+            permalink = folder.permalink,
+            created = folder.created,
+            modified = folder.modified
+        )
+    }
 
     fun FolderLocalEntity.toModel(): Folder =
         Folder(
--- shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/datasources/IFolderLocalDataSource.kt
+++ shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/datasources/IFolderLocalDataSource.kt
@@ -1,7 +1,9 @@
 package com.kiteworks.kiteworkskmmpoc.data.folder.datasources
 
 import com.kiteworks.kiteworkskmmpoc.domain.folder.Folder
+import kotlinx.coroutines.flow.Flow
 
 interface IFolderLocalDataSource {
-    suspend fun getAllFolders(): List<Folder>
+    fun getAllFolders(): Flow<List<Folder>>
+    fun insertFolder(folder: Folder)
 }

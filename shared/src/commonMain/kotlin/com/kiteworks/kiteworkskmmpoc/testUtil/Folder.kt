63900555d1fe4deef09de81182eae0d536f4de41
joragua <jorgeagurec@gmail.com>
2025-03-05 10:42:36 +0100

    test: add tests for all methods from FolderRepository

---
+++ shared/src/commonMain/kotlin/com/kiteworks/kiteworkskmmpoc/testUtil/Folder.kt
@@ -0,0 +1,16 @@
+package com.kiteworks.kiteworkskmmpoc.testUtil
+
+import com.kiteworks.kiteworkskmmpoc.domain.folder.Folder
+
+val FOLDER = Folder(
+    id = 717262,
+    name = "Test",
+    userId = 15619,
+    isShared = false,
+    path = "Test",
+    parentId = 707268,
+    type = "d",
+    permalink = "https://mobile.kiteworks.com/w/test",
+    created = "2024-10-09T07:16:53+0000",
+    modified = "2025-01-10T13:37:39+0000"
+)
---
+++ shared/src/commonTest/kotlin/com/kiteworks/kiteworkskmmpoc/data/folder/repository/FolderRepositoryTest.kt
@@ -0,0 +1,58 @@
+package com.kiteworks.kiteworkskmmpoc.data.folder.repository
+
+import com.kiteworks.kiteworkskmmpoc.data.folder.datasources.IFolderLocalDataSource
+import com.kiteworks.kiteworkskmmpoc.data.folder.datasources.IFolderRemoteDataSource
+import com.kiteworks.kiteworkskmmpoc.testUtil.FOLDER
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.kodein.mock.Mock
+import org.kodein.mock.generated.injectMocks
+import org.kodein.mock.tests.TestsWithMocks
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FolderRepositoryTest: TestsWithMocks() {
+    override fun setUpMocks() = mocker.injectMocks(this)
+
+    @Mock
+    lateinit var folderLocalDataSource: IFolderLocalDataSource
+    @Mock
+    lateinit var folderRemoteDataSource: IFolderRemoteDataSource
+
+    private val folderRepository by withMocks { FolderRepository(folderLocalDataSource, folderRemoteDataSource) }
+
+
+    @Test
+    fun `getAllFolders returns a list of Folder`() = runTest {
+        every {
+            folderLocalDataSource.getAllFolders()
+        } returns flowOf(listOf(FOLDER))
+
+        val listOfFolders = folderRepository.getAllFolders().first()
+        assertEquals(listOf(FOLDER),listOfFolders)
+
+        verify {
+            folderLocalDataSource.getAllFolders()
+        }
+    }
+
+    @Test
+    fun `refreshAllFolders refreshes all folders that are in the root correctly`() = runTest {
+        everySuspending {
+            folderRemoteDataSource.refreshFolders()
+        } returns listOf(FOLDER)
+
+        every {
+            folderLocalDataSource.insertFolder(FOLDER)
+        } returns Unit
+
+        folderRepository.refreshFolders()
+
+        verifyWithSuspend {
+            folderRemoteDataSource.refreshFolders()
+            folderLocalDataSource.insertFolder(FOLDER)
+        }
+
+    }
+}
